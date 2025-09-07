3d79c3b3d77200f6a7c2bf26edce94c7add0ea9a
robertgruevski <robertgruevski@outlook.com>
2025-09-07 19:36:50 -0400

    Login component

--- ClientApp/src/app/account/account.service.ts
+++ ClientApp/src/app/account/account.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { LoginModel } from '../shared/models/account/login.model';
+import { UserModel } from '../shared/models/account/user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-  
+  apiUrl = environment.apiUrl;
+
+  private http = inject(HttpClient);
+  private route = inject(Router);
+
+  constructor() {}
+
+  login(model: LoginModel) {
+    return this.http.post<UserModel>(this.apiUrl + 'account/login', model, {
+      withCredentials: true,
+    });
+  }
 }
--- ClientApp/src/app/account/login/login.ts
+++ ClientApp/src/app/account/login/login.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ValidationMessage } from '../../shared/components/errors/validation-message/validation-message';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, ValidationMessage],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
-export class Login {
+export class Login implements OnInit {
+  form: FormGroup = new FormGroup({});
+  submitted = false;
+  errorMessages: string[] = [];
+  returnUrl: string | null = null;
 
+  private formBuilder = inject(FormBuilder);
+  private route = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
+  private accountService = inject(AccountService);
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.form = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  login() {
+    this.submitted = true;
+    this.errorMessages = [];
+
+    if (this.form.valid) {
+      this.accountService.login(this.form.value).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
+    }
+  }
 }
