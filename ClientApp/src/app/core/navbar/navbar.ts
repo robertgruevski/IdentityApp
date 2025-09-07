dd75bc3f9045d53691076f6189823658855329f2
robertgruevski <robertgruevski@outlook.com>
2025-09-07 15:53:00 -0400

    Angular routing and navbar-footer modifcation

--- ClientApp/src/app/core/navbar/navbar.ts
+++ ClientApp/src/app/core/navbar/navbar.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLinkActive, RouterLink, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
+  collapsed = true;
 
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
+  }
 }
